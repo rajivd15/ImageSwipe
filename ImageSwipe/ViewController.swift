2410fce0bbc3860457effef405d4a74208b8827d
Rajiv Deshmukh <rdeshmukh@walmartlabs.com>
2019-03-21 15:43:15 -0700

    Suggestiosn for Auto Scrolling - Timer vs scrollToPage

--- ImageSwipe/ViewController.swift
+++ ImageSwipe/ViewController.swift
@@ -8,87 +8,56 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    var imageView: UIImageView = {
-       var imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.isUserInteractionEnabled = true
-        return imageView
+class ViewController: UIViewController, UIScrollViewDelegate {
+    
+    let imageArray: [UIImage] = (1...5).map { UIImage(named: "image\($0)")! }
+    var counter = 1
+    let scrollView: UIScrollView = {
+        let scroll = UIScrollView()
+        scroll.isPagingEnabled = true
+        scroll.showsVerticalScrollIndicator = false
+        scroll.showsHorizontalScrollIndicator = false
+        scroll.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        return scroll
     }()
     
-    var imageIndex = 0
-    let arrayOfImages = ["image1", "image2", "image3", "image4", "image5"]
-    var counter = 1
-    var timer = Timer()
-
-    let loadingImages: [UIImage] = (1...5).map { UIImage(named: "image\($0)")! }
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.view.addSubview(self.imageView)
-        setupConstraints()
-        UIView.animate(withDuration: 1.0) {
-            self.doSomeAnimation()
-        }
-//        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(doSomeAnimation), userInfo: nil, repeats: true)
-//        self.imageView.image = UIImage(named: arrayOfImages[imageIndex])
-//
-//        self.imageView.animationImages = loadingImages
-//        self.imageView.animationDuration = 10.0
-//        self.imageView.startAnimating()
-        
-//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.imageSwipped(_:)))
-//        swipeLeft.direction = .left
-//        self.imageView.addGestureRecognizer(swipeLeft)
-//
-//        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.imageSwipped))
-//        swipeRight.direction = .right
-//        self.imageView.addGestureRecognizer(swipeRight)
-        
-        
+        self.view.addSubview(self.scrollView)
+        setupImages(imageArray)
+        let timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(self.update), userInfo: nil, repeats: true)
+//        self.scrollToPage(page: 2, animated: true)
     }
-    
-    @objc func doSomeAnimation() {
-        //I have four pngs in my project, which are named frame1.png ... and so on
-        
-        
-        if counter == 5 {
-            
-            counter = 1
-            
-        }else {
-            
+    // must be internal or public.
+    @objc func update() {
+        UIView.animate(withDuration: 1.0) {
+            self.scrollView.contentOffset.x = self.scrollView.frame.width * CGFloat(self.counter)
+        }
+        print("Done")
+        if counter == imageArray.count {
+            counter = 0
+        } else {
             counter += 1
         }
+    }
+    
+    func scrollToPage(page: Int, animated: Bool) {
         
-        self.imageView.image = UIImage(named: "image\(counter)")
     }
     
-    @objc func imageSwipped(_ gesture: UIGestureRecognizer) {
-        if let swipeGesture = gesture as? UISwipeGestureRecognizer {
-            switch swipeGesture.direction {
-            case UISwipeGestureRecognizer.Direction.left:
-                if imageIndex < arrayOfImages.count - 1 {
-                    imageIndex += 1
-                    self.imageView.image = UIImage(named: arrayOfImages[imageIndex])
-                }
-            case UISwipeGestureRecognizer.Direction.right:
-                if imageIndex > 0 {
-                    imageIndex -= 1
-                    self.imageView.image = UIImage(named: arrayOfImages[imageIndex])
-                }
-            default:
-                print("I am here")
-            }
+    func setupImages(_ images: [UIImage]) {
+        for i in 0..<images.count {
+            let imageView = UIImageView()
+            imageView.image = images[i]
+            let xPosition = UIScreen.main.bounds.width * CGFloat(i)
+            imageView.frame = CGRect(x: xPosition, y: 0, width: scrollView.frame.width, height: scrollView.frame.height)
+            imageView.contentMode = .scaleAspectFit
+            
+            scrollView.contentSize.width = scrollView.frame.width * CGFloat(i + 1)
+            scrollView.addSubview(imageView)
+            scrollView.delegate = self
         }
-    }
-    
-    private func setupConstraints() {
-        NSLayoutConstraint.activate([self.imageView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 20.0),
-                                     self.imageView.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 50.0),
-                                     self.imageView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -20.0),
-                                     self.imageView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor, constant: -50.0)])
     }
 }
 
