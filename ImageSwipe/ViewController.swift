a8754bd5376e8dc8ceb58b908c4a812b62130002
Rajiv Deshmukh <rdeshmukh@walmartlabs.com>
2019-03-21 13:02:12 -0700

    Animation Code commented

--- ImageSwipe/ViewController.swift
+++ ImageSwipe/ViewController.swift
@@ -19,21 +19,50 @@ class ViewController: UIViewController {
     
     var imageIndex = 0
     let arrayOfImages = ["image1", "image2", "image3", "image4", "image5"]
-    
+    var counter = 1
+    var timer = Timer()
+
+    let loadingImages: [UIImage] = (1...5).map { UIImage(named: "image\($0)")! }
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.addSubview(self.imageView)
         setupConstraints()
-        self.imageView.image = UIImage(named: arrayOfImages[imageIndex])
+        UIView.animate(withDuration: 1.0) {
+            self.doSomeAnimation()
+        }
+//        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(doSomeAnimation), userInfo: nil, repeats: true)
+//        self.imageView.image = UIImage(named: arrayOfImages[imageIndex])
+//
+//        self.imageView.animationImages = loadingImages
+//        self.imageView.animationDuration = 10.0
+//        self.imageView.startAnimating()
         
-        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.imageSwipped(_:)))
-        swipeLeft.direction = .left
-        self.imageView.addGestureRecognizer(swipeLeft)
+//        let swipeLeft = UISwipeGestureRecognizer(target: self, action: #selector(self.imageSwipped(_:)))
+//        swipeLeft.direction = .left
+//        self.imageView.addGestureRecognizer(swipeLeft)
+//
+//        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.imageSwipped))
+//        swipeRight.direction = .right
+//        self.imageView.addGestureRecognizer(swipeRight)
         
-        let swipeRight = UISwipeGestureRecognizer(target: self, action: #selector(self.imageSwipped))
-        swipeRight.direction = .right
-        self.imageView.addGestureRecognizer(swipeRight)
+        
+    }
+    
+    @objc func doSomeAnimation() {
+        //I have four pngs in my project, which are named frame1.png ... and so on
+        
+        
+        if counter == 5 {
+            
+            counter = 1
+            
+        }else {
+            
+            counter += 1
+        }
+        
+        self.imageView.image = UIImage(named: "image\(counter)")
     }
     
     @objc func imageSwipped(_ gesture: UIGestureRecognizer) {
